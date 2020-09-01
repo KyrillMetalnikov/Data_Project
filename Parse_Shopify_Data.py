8d40e0ddd418cb7131e55ac3898f67b087ed8c21
Kyrill Metalnikov <kmetalnikov@hotmail.com>
2020-08-31 20:54:54 -0700

    Added statistical output about number of rows removed, and the percentage of rows removed.

--- Parse_Shopify_Data.py
+++ Parse_Shopify_Data.py
@@ -21,6 +21,7 @@ def get_excel_data(name: str):
 def remove_row_by_total_items(max_items: int, json_file: dict) -> dict:
     """
     Removes row from a dictionary if total_items key has a higher value than inputted max_items.
+
     :param max_items: type int, maximum amount of items allowed
     :param json_file: type dict, the json object being parsed
     :return: A filtered dictionary.
@@ -39,6 +40,7 @@ def remove_row_by_total_items(max_items: int, json_file: dict) -> dict:
 def remove_row_by_unit_cost(max_cost: int, json_file: dict) -> dict:
     """
     Removes row from a dictionary if unit cost has a higher value than inputted max_cost.
+
     :param max_cost: type int, maximum cost allowed
     :param json_file: type dict, the json object being parsed
     :return: A filtered dictionary.
@@ -54,12 +56,29 @@ def remove_row_by_unit_cost(max_cost: int, json_file: dict) -> dict:
     return json_file_copy
 
 
+def find_average_order_cost(json_file: dict) -> float:
+    """
+    Find the average order cost.
+
+    :param json_file: Type dict, the Json file being parsed.
+    :return: Type float, the average cost of an order.
+    """
+    total_cost = 0
+    for row in json_file:
+        total_cost += row["order_amount"]
+    return total_cost / len(json_file)
+
+
 def main():
     get_excel_data("2019 Winter Data Science Intern Challenge Data Set.xlsx")  # data sheet is named Sheet1.json
     with open("Sheet1.json") as file:
         spreadsheet = json.load(file)
     once_filtered_spreadsheet = remove_row_by_total_items(MAX_TOTAL_ITEMS, spreadsheet)
     twice_filtered_spreadsheet = remove_row_by_unit_cost(MAX_COST, once_filtered_spreadsheet)
+    print("Total average order cost = $" + str(find_average_order_cost(twice_filtered_spreadsheet)))
+    print("Number of rows removed = " + str(len(spreadsheet) - len(twice_filtered_spreadsheet)))
+    print("Percentage of rows removed = " +
+          str((len(spreadsheet) - len(twice_filtered_spreadsheet)) / len(spreadsheet)) + "%")
 
 
 if __name__ == '__main__':
